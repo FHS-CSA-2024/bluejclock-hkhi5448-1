bbd97a43558ec24efceae14afcfd73b146f150a8
hkhi5448 <hk07.khinda@gmail.com>
2024-12-12 22:37:29 -0800

    Update ClockDriver.java

--- src/main/java/ClockDriver.java
+++ src/main/java/ClockDriver.java
@@ -1,3 +1,5 @@
+package src.main.java;
+
 
 public class ClockDriver
 {
@@ -14,13 +16,31 @@ public class ClockDriver
         //  * Tick test for 01:59 to 02:00
         //  * Tick test for 09:59 to 10:00
         //  * Tick test for 23:59 to 00:00
+        System.out.print("Testing clock display:");
+        ClockDisplay test1 = new ClockDisplay();
+        String test1Output = test1.getTime();
+        System.out.println("\tEmpty Con. Test - " + test1Output);
         
+        ClockDisplay test2 = new ClockDisplay(2, 45);
+        String test2Output = test2.getTime();
+        System.out.println("\t2 Arg Con. Test - " + test2Output);
+        
+        test2.setTime(5, 15);
+        String test3Output = test2.getTime();
+        System.out.println("\t Set Time Test - " + test3Output);
+        
+        ClockDisplay tickTest1 = new ClockDisplay(3, 32);
+        String tickTest1Output = tickTest1.getTime();
+        System.out.println("\tTickTest1 Before increment - " + tickTest1Output);
+        tickTest1.timeTick();
+        tickTest1Output = tickTest1.getTime();
+        System.out.println("\tTickTest1 After inrement - " + tickTest1Output);
         
         //Implement tests for your ClockDisplaySeconds
         //  * Test that you can do the basics
         //      * Empty constructor & read time
         //      * Constructor w/ time given & read time
-        //      * Set time
+        //      * Set time  
         //      * Read time
         //      * Tick time
         //  * Tick test for 03:32:59 to 03:33:00
